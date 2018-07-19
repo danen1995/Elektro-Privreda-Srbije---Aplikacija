7725be4ed353c24198ab38fc52c5d17a6e674601
Dane <danen1995@gmail.com>
2018-07-19 14:42:51 +0200

    Commit - proba

--- KupciBS/src/main/java/com/example/demo/ProjekatDaneApplication.java
+++ KupciBS/src/main/java/com/example/demo/ProjekatDaneApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-
+//Main klasa
 public class ProjekatDaneApplication {
 
 	public static void main(String[] args) {
